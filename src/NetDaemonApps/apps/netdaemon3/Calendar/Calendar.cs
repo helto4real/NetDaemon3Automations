8777ead178c01a38c7462e2adaf41c13739e4f30
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-05-04 22:48:14 +0200

    Fixed the google calendar integration to work again

--- src/NetDaemonApps/apps/netdaemon3/Calendar/Calendar.cs
+++ src/NetDaemonApps/apps/netdaemon3/Calendar/Calendar.cs
@@ -1,25 +1,27 @@
-using NetDaemon.Extensions.Tts;
-
 /// <summary>
 ///     Pushes important calendar events to TTS.
 /// </summary>
-// [NetDaemonApp]
+[NetDaemonApp]
 public class GoogleCalendarManager
 {
-    public GoogleCalendarManager(IEntities entities, ITextToSpeechService tts)
+    public GoogleCalendarManager(Entities entities, ITextToSpeechService tts, ILogger<GoogleCalendarManager> logger)
     {
         // entities.InputSelect.HouseModeSelect.StateChanges()
         //     .Where(e => e.New?.State == "Natt")
         //     .Subscribe(_ => tts.Speak("media_player.huset", "Tomas, kom ihåg att ta din medicin!", "google_cloud_say"));
-        // entities.Calendar.TaUtSopor
-        //     .WhenTurnsOn(s =>
-        //         {
-        //             tts.Speak("media_player.huset", "Viktigt meddelande", "google_cloud_say"); // Important message
-        //             if (s.New?.Attributes?.Message is not null)
-        //                 tts.Speak("media_player.huset", s.New.Attributes.Message, "google_cloud_say");
-        //             if (s.New?.Attributes?.Description is not null)
-        //                 tts.Speak("media_player.huset", s.New.Attributes.Description, "google_cloud_say");
-        //         }
-        //     );
+        entities.Calendar.FamiljenHellstrom
+            .WhenTurnsOn(s =>
+                {
+                    logger.LogInformation("Calendar event: {Event}", s.New?.Attributes?.Message);               
+                    if (s.New?.Attributes?.Message is not null && s.New.Attributes.Message.Contains("sopor"))
+                    {
+                        tts.Speak("media_player.huset", "Viktigt meddelande", "google_translate_say"); // Important message
+                        if (s.New?.Attributes?.Message is not null)
+                            tts.Speak("media_player.huset", s.New.Attributes.Message, "google_translate_say");
+                        if (s.New?.Attributes?.Description is not null)
+                            tts.Speak("media_player.huset", s.New.Attributes.Description, "google_translate_say");
+                    }
+                }
+            );
     }
 }
